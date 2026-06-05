36f8730ae17c13166f38ca59893ce193677e9d23
Your Name <you@example.com>
2026-06-05 08:14:43 +0100

    fix CORS allow all

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
